a430fd7a38c63aedced0226cd763146350db2b84
Priyal Sarda <priyal.sarda2005@gmail.com>
2025-11-11 22:45:29 +0530

    login route added

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.post("register", async (req, res) => {
 
         // if user exists then error
         if (user) {
-            return res.status(400).json({message: "User already exists"});
+            return res.status(422).json({message: "User already exists"});
         }
 
         // else, hash password and create user
@@ -46,5 +46,30 @@ app.post("register", async (req, res) => {
 
 // login
 app.post("login", async (req, res) => {
+    try {
+        const {email, password} = req.body;
+        const user = await prisma.user.findUnique({
+            where: {email}
+        })
 
+        if (!user) {
+            return res.status(422).json({message: "User does not exist"})
+        }
+
+        const isPasswordMatch = await bcrypt.compare(password, user.password);
+        if (!isPasswordMatch) {
+            return res.status(401).json({message: "Invalid credentials"});
+        }
+        else {
+            const token = jwt.sign({id: user.id, email: user.email}, process.env.SECRET_KEY);
+            return res.status(200).json({
+                message: "Login successfuk",
+                token: token,
+                email: user.email
+            })
+        }
+    }
+    catch (error) {
+        return res.status(500).json({error: "Internal Server Error"});
+    }
 });
