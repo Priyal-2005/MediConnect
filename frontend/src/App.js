b59ad816f8331e29ce794f9624a9f461d7babc10
Priyal Sarda <priyal.sarda2005@gmail.com>
2025-11-11 23:21:19 +0530

    appjs basic frontend added

---
+++ frontend/src/App.js
@@ -0,0 +1,35 @@
+import React, {useState} from "react";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Dashboard from "./components/Dashboard";
+
+function App() {
+  const [page, setPage] = useState("login");
+  const [user, setUser] = useState(null);
+
+  return (
+    <>
+      {page === "login" && (
+        <Login 
+        onLogin={(userData) => {
+          setUser(userData);
+          setPage("dashboard");
+        }}
+        goToRegister={() => setPage("register")}/>
+      )}
+      {page === "register" && (
+        <Register
+        onRegister={() => setPage("login")}
+        goToLogin={() => setPage("login")}/>
+      )}
+      {page === "dashboard" && user && (
+        <Dashboard user={user} onLogout={() => {
+          setUser(null);
+          setPage("login");
+        }}/>
+      )}
+    </>
+  )
+}
+
+export default App;
